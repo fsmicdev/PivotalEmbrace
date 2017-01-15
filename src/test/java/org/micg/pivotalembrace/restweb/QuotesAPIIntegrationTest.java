55171b075c74b9bfb340c600da9308e9e3cf84c5
fsmicdev <micqdig@gmail.com>
2017-01-15 19:44:08 +1000

    Beginning of REST (end-to-end) integration tests, for QuotesAPI.

--- src/test/java/org/micg/pivotalembrace/restweb/QuotesAPIIntegrationTest.java
+++ src/test/java/org/micg/pivotalembrace/restweb/QuotesAPIIntegrationTest.java
@@ -13,6 +13,9 @@ import static org.micg.pivotalembrace.util.IntegrationTestConstants.BASE_URL;
 import static org.micg.pivotalembrace.util.IntegrationTestConstants.SERVER_PORT;
 import static org.micg.pivotalembrace.util.IntegrationTestConstants.APP_BASE_PATH;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 /**
  *
  *
@@ -32,8 +35,39 @@ public class QuotesAPIIntegrationTest {
 
     @Test
     public void getAllQuotes_success_200StatusAndAllQuotesReturned() {
-        // get("/").then().assertThat().statusCode(200);
+        get("/").then().assertThat().statusCode(200);
     }
 
+    @Test
+    public void getQuote_nonPlausibleQuoteId_400Status() {
+        given().
+                pathParam("id", -1).
+                when().
+                get("/quote/{id}").
+                then().
+                statusCode(400);
+    }
+
+    @Test
+    public void getQuote_nonExistingQuoteId_404Status() {
+        given().
+                pathParam("id", 1892341873).
+                when().
+                get("/quote/{id}").
+                then().
+                statusCode(404);
+    }
+
+    @Test
+    public void getQuote_existingQuoteId_200StatusAndQuoteReturned() {
+        given().
+                pathParam("id", 1).
+                when().
+                get("/quote/{id}").
+                then().
+                statusCode(200).
+                body("person", equalTo("Albert Einstein")).
+                body("quote", equalTo("Anyone who has never made a mistake has never tried anything new."));
+    }
 
 }
